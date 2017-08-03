8a888124ee2b3f0607dee8c291a0e5f3c5f46df0
Matthew Clark <mclark4386@gmail.com>
2017-08-03 17:08:32 -0500

    *now properly using the event channel for ios

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_driver/driver_extension.dart';
-import 'package:flutter/services.dart';
 import 'package:flocation/flocation.dart';
+import 'dart:async';
 
 void main() {
   // Enable integration testing with the Flutter Driver extension.
@@ -16,84 +16,39 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-  List<double> _location = [];
-
-  @override
-  initState() {
-    super.initState();
-    initPlatformState();
-    initLocationState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await Flocation.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted)
-      return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  initLocationState() async {
-    List<double> location;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      location = await Flocation.location;
-    } on PlatformException {
-      location = Float64List(2);
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted)
-      return;
-
-    setState(() {
-      _location = location;
-    });
-  }
-
-  Widget viewLocation(){
-    if(_location.length <= 0){
-      return new Text("No location");
-    }else{
-      double long = _location[0];
-      double lat = _location[1];
-      return new Text('Location($long , $lat)');
-    }
-  }
+  List<double> _locationValues = [];
+  List<StreamSubscription<dynamic>> _streamSubscriptions = <StreamSubscription<dynamic>>[];
 
   @override
   Widget build(BuildContext context) {
+    final List<String> location = _locationValues?.map((double v) => v.toStringAsFixed(3))?.toList();
     return new MaterialApp(
       home: new Scaffold(
         appBar: new AppBar(
           title: new Text('Plugin example app'),
         ),
-        body: new Column(
+        body: new Center( child: new Column(
 	  children: [new Center(
-            child: new Text('Running on: $_platformVersion\n'),
-          ),
-	  new Center(
-	    child: viewLocation(),
-          ),]
-	),
+            child: new Text('Running on: $location\n'),
+                          ),],
+                                            )
       ),
-    );
+                         ));
+  }
+
+  @override
+  dispose(){
+    super.dispose();
+    for(StreamSubscription<dynamic> sub in _streamSubscriptions){
+      sub.cancel();
+    }
+  }
+
+  @override
+  initState(){
+    super.initState();
+    _streamSubscriptions.add(Flocation.locationEvents.listen((LocationEvent event) {
+        setState((){ _locationValues = <double>[event.latitude, event.longitude, event.elevation];});
+      }));
   }
 }
--- lib/flocation.dart
+++ lib/flocation.dart
@@ -2,13 +2,30 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
-class Flocation {
-  static const MethodChannel _channel =
-      const MethodChannel('motionmobs.com/flocation');
+class LocationEvent{
+  final double latitude;
+  final double longitude;
+  final double elevation;
 
-  static Future<String> get platformVersion =>
-      _channel.invokeMethod('getPlatformVersion');
-      
-  static Future<List<double>> get location =>
-      _channel.invokeMethod('getLocation');
+  LocationEvent(this.latitude, this.longitude, this.elevation);
+
+  LocationEvent.fromList(List<double> list) : this(list[0], list[1], list[2]);
+
+  @override
+    String toString() => '[LocationEvent (lat: $latitude, long: $longitude, elev: $elevation)]';
+}
+
+class Flocation {
+  static Stream<LocationEvent> _locationEvents;
+  static const EventChannel _locationEventChannel =
+      const EventChannel('motionmobs.com/flocation');
+
+  static Stream<LocationEvent> get locationEvents {
+    if(_locationEvents == null) {
+       _locationEvents = _locationEventChannel
+         .receiveBroadcastStream()
+         .map((x) => new LocationEvent.fromList(x));
+    }
+    return _locationEvents;
+  }
 }
