daffa323e73bb02ecea7ef295bad91c9556b10b8
Matthew Clark <mclark4386@gmail.com>
2017-08-04 16:12:25 -0500

    *android is to pairity with ios

--- android/src/main/kotlin/com/yourcompany/flocation/FlocationPlugin.kt
+++ android/src/main/kotlin/com/yourcompany/flocation/FlocationPlugin.kt
@@ -1,27 +1,58 @@
 package com.yourcompany.flocation
 
-import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.MethodCall
+import android.os.AsyncTask
+import java.lang.Thread
+import io.flutter.plugin.common.EventChannel
+import io.flutter.plugin.common.EventChannel.StreamHandler
+import io.flutter.plugin.common.EventChannel.EventSink
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
-class FlocationPlugin(): MethodCallHandler {
+class FlocationPlugin(): StreamHandler {
   companion object {
     @JvmStatic
     fun registerWith(registrar: Registrar): Unit {
-      val channel = MethodChannel(registrar.messenger(), "motionmobs.com/flocation")
-      channel.setMethodCallHandler(FlocationPlugin())
+        val LocationID = "motionmobs.com/flocation"
+        val channel = EventChannel(registrar.messenger(), LocationID)
+      channel.setStreamHandler(FlocationPlugin())
     }
   }
 
-  override fun onMethodCall(call: MethodCall, result: Result): Unit {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-      }else if (call.method.equals("getLocation")){
-      result.success([23.1, 645.234])
-    } else {
-      result.notImplemented()
+
+
+   override fun onListen(arguments: Any?, eventSink: EventSink?){
+        //TODO: stream location events!
+
+       TestEvents().execute(eventSink);
     }
-  }
+
+   override fun onCancel(arguments: Any?){
+        //TODO: clean up!
+    }
+
+    inner class TestEvents: AsyncTask<EventSink?, Boolean, Boolean>() {
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+        }
+
+        override fun doInBackground(vararg events: EventSink?):Boolean {
+            for (eventSink in events) {
+                if(eventSink != null) {
+                    for (i in 0..99) {
+                        val lat = i.toDouble();
+                        val long = i.toDouble();
+                        val elev = i.toDouble();
+                        eventSink?.success(listOf(lat, long, elev))
+                        Thread.sleep(1_000)
+                    }
+                    return true
+                }
+            }
+            return false
+        }
+
+        override fun onPostExecute(result: Boolean) {
+            super.onPostExecute(result)
+        }
+    }
 }
