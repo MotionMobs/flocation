2392df540af15b8a1b0ffb17b01f806ab9ebef89
Matthew Clark <mclark4386@gmail.com>
2017-08-04 16:16:19 -0500

    *cleanup spacing

--- android/src/main/kotlin/com/yourcompany/flocation/FlocationPlugin.kt
+++ android/src/main/kotlin/com/yourcompany/flocation/FlocationPlugin.kt
@@ -7,37 +7,35 @@ import io.flutter.plugin.common.EventChannel.StreamHandler
 import io.flutter.plugin.common.EventChannel.EventSink
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
-class FlocationPlugin(): StreamHandler {
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar): Unit {
-        val LocationID = "motionmobs.com/flocation"
-        val channel = EventChannel(registrar.messenger(), LocationID)
-      channel.setStreamHandler(FlocationPlugin())
+class FlocationPlugin() : StreamHandler {
+    companion object {
+        @JvmStatic
+        fun registerWith(registrar: Registrar): Unit {
+            val LocationID = "motionmobs.com/flocation"
+            val channel = EventChannel(registrar.messenger(), LocationID)
+            channel.setStreamHandler(FlocationPlugin())
+        }
     }
-  }
 
-
-
-   override fun onListen(arguments: Any?, eventSink: EventSink?){
+    override fun onListen(arguments: Any?, eventSink: EventSink?) {
         //TODO: stream location events!
 
-       TestEvents().execute(eventSink);
+        TestEvents().execute(eventSink);
     }
 
-   override fun onCancel(arguments: Any?){
+    override fun onCancel(arguments: Any?) {
         //TODO: clean up!
     }
 
-    inner class TestEvents: AsyncTask<EventSink?, Boolean, Boolean>() {
+    inner class TestEvents : AsyncTask<EventSink?, Boolean, Boolean>() {
 
         override fun onPreExecute() {
             super.onPreExecute()
         }
 
-        override fun doInBackground(vararg events: EventSink?):Boolean {
+        override fun doInBackground(vararg events: EventSink?): Boolean {
             for (eventSink in events) {
-                if(eventSink != null) {
+                if (eventSink != null) {
                     for (i in 0..99) {
                         val lat = i.toDouble();
                         val long = i.toDouble();
